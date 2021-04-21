3532abc1408df744eb5442bbccbdd89d3d965d17
Aaron Yang <zhenhuayang@outlook.com>
2021-04-20 22:53:33 -0500

    switching questions works

--- boardgamers/form/js/formConfig.js
+++ boardgamers/form/js/formConfig.js
@@ -3,54 +3,63 @@ const questions = [
         label: "What is your favorite board game?",
         name: "gameName",
         tag: "input",
-        type: "text"
+        type: "text",
+        required: true
     },
     {
         label: "What is your most memorable experience playing this game?",
         name: "memory",
         tag: "textarea",
-        type: ""
+        type: "",
+        required: false
     },
     {
         label: "What do you look forward to most in playing this game?",
         name: "forward",
         tag: "textarea",
-        type: ""
+        type: "",
+        required: false
     },
     {
         label: "Give a short elevator pitch of how this game is played. OR Describe how the game is played in 10 words or less.",
         name: "pitch",
         tag: "textarea",
-        type: ""
+        type: "",
+        required: false
     },
     {
         label: "Share a picture of the game.",
         name: "picture",
         tag: "input",
-        type: "file"
+        type: "file",
+        required: false
     },
     {
         label: "What genres of board games do you usually play?",
         name: "genres",
         tag: "input",
-        type: ""
+        type: "",
+        required: false
     },
     {
         label: "Is there a board game you would like to play, but haven’t had the chance yet?",
         name: "wanted",
         tag: "input",
-        type: ""
+        type: "",
+        required: false
     },
     {
         label: "What's your name?",
         name: "userName",
         tag: "input",
-        type: ""
+        type: "",
+        required: false
     },
     {
         label: "What is your location? ",
         name: "location",
         tag: "select",
-        type: ""
+        type: "",
+        required: false
     }
 ]
--- boardgamers/form/js/index.js
+++ boardgamers/form/js/index.js
@@ -1,19 +1,102 @@
 const formContainer = document.querySelector(".form-container");
+const btnPrev = document.querySelector("#btnPrev");
+const btnNext = document.querySelector("#btnNext");
 
-questionIndex = 3
+let questionIndex = 0;
+let game = {};
 
-formContainer.append(
-    buildDom({
-        type: "label",
-        props: {
-            htmlFor: questions[questionIndex].name,
-            innerHTML: questions[questionIndex].label
+loadQuestions(questions);
+const allQuestions = document.querySelectorAll(".question");
+allQuestions[0].classList.remove("hide");
+
+btnNext.addEventListener("click", e => {
+    questionIndex++;
+    if (questionIndex >= questions.length) {
+        // TODO: submit form
+    } else {
+        btnPrev.disabled = false;
+        switchQuestion(questionIndex, 1);
+        if (questionIndex === questions.length - 1) {
+            e.target.innerHTML = "Submit";
         }
-    }),
-    buildDom({
-        type: questions[questionIndex].tag,
-        props: {
-            name: questions[questionIndex].name,
-            type: questions[questionIndex].type
+    }
+    
+});
+
+btnPrev.addEventListener("click", e => {
+    questionIndex--;
+    if (questionIndex < 0) {
+        questionIndex = 0;
+    } else {
+        switchQuestion(questionIndex, -1);
+        if (questionIndex === 0) {
+            e.target.disabled = true;
+        } else if (questionIndex === questions.length - 2) {
+            btnNext.innerHTML = "Next";
         }
-    }));
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * 
+ * @param {Number} i the index of the next question.
+ * @param {Number} order 1 for the next question, -1 for the previous question
+ */
+function switchQuestion(i, order) {
+    if (order === 1) {
+        allQuestions[i - 1].classList.add("hide");
+    } else if (order === -1) {
+        allQuestions[i + 1].classList.add("hide"); 
+    }
+    allQuestions[i].classList.remove("hide");
+}
+
+function loadQuestions(questionObj){
+
+    for ( let i = 0; i < questionObj.length; i++) {
+        formContainer.append(
+            buildDom({
+                type: "div",
+                props: {
+                    className: "question hide"
+                },
+                children: [{
+                    type: "label",
+                    props: {
+                        htmlFor: questionObj[i].name,
+                        innerHTML: (i + 1) + '. ' + questionObj[i].label
+                    }
+                }, {
+                    type: questionObj[i].tag,
+                    props: {
+                        name: questionObj[i].name,
+                        type: questionObj[i].type,
+                        className: "full-width",
+                        required: questionObj[i].name.required
+                    },
+                    events: {
+                        change: e => {
+                            game[questionObj[i].name] = e.target.value;
+                            console.log(game);
+                        }
+                    }
+                }]
+            })
+        );
+    }
+    
+}
